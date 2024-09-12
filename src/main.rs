01b899eb7e59fdae8b3f026d8573cd8c6bec8d0c
liamm <131229216+l-c-mcc@users.noreply.github.com>
2024-09-12 11:48:15 -0700

    clean up webpage

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::collections::HashMap;
 
 const SCREEN_LEN: f32 = 300. * SCALE;
 const N_TILES: usize = 8;
-const SCALE: f32 = 2.5;
+const SCALE: f32 = 2.;
 const SQUARE_LEN: f32 = 32. * SCALE;
 const TILE_GAP: f32 = 2. * SCALE;
 const TILE_DIS: f32 = TILE_GAP + SQUARE_LEN;
