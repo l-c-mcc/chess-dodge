d1fa4e425f87e0317c6626804075f9ca1b1772e9
liamm <131229216+l-c-mcc@users.noreply.github.com>
2024-09-12 08:48:49 -0700

    allow spawns in last column again

--- src/main.rs
+++ src/main.rs
@@ -448,7 +448,7 @@ fn spawn_opp_pieces(
             let mut spawn_locations = vec![];
             let top_row = &board.board[0];
             // todo: player can avoid all danger by living in top row
-            for (elem, tile) in top_row.iter().enumerate().take(N_TILES - 1) {
+            for (elem, tile) in top_row.iter().enumerate().take(N_TILES) {
                 if tile == &TileType::Empty {
                     spawn_locations.push(elem);
                 }
